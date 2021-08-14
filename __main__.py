850df14bd63ad1c862f3d7ea5fcc9651a28279eb
Denis 070 - IP5 14 <azzrael@bk.ru>
2021-08-14 15:35:56 +0300

    init

---
+++ __main__.py
@@ -0,0 +1,42 @@
+import os
+
+import httplib2
+from googleapiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+
+import creds
+
+
+def get_service_simple():
+    """
+    Могу читать таблицы созданные под своим же аккаунтом
+    :return:
+    """
+    return build('sheets', 'v4', developerKey=creds.api_key)
+
+
+def get_service_sacc():
+    """
+    Могу читать и (возможно) писать в таблицы кот. выдан доступ
+    для сервисного аккаунта приложения
+
+    sacc-1@privet-yotube-azzrael-code.iam.gserviceaccount.com
+
+    :return:
+    """
+    creds_json = os.path.dirname(__file__) + "/creds/sacc1.json"
+    scopes = ['https://www.googleapis.com/auth/spreadsheets']
+
+    creds_service = ServiceAccountCredentials.from_json_keyfile_name(creds_json, scopes).authorize(httplib2.Http())
+    return build('sheets', 'v4', http=creds_service)
+
+
+sheet_id = "1IfE0sBAkKvhB6F8zHkEozEE0jpwhAU_G4UubwKTV1Bk"
+
+# service = get_service_simple()
+service = get_service_sacc()
+sheet = service.spreadsheets()
+
+# https://developers.google.com/resources/api-libraries/documentation/sheets/v4/python/latest/sheets_v4.spreadsheets.html
+resp = sheet.values().get(spreadsheetId=sheet_id, range="Лист1").execute()
+print(resp)
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Код для видео на Azzrael Code о работе с Google Sheets API
+
+Для каждого видео своя ветка - просто переключайся чтобы посмотреть код 
+соотв. видео.
+
+В первом видео я покажу как читать Google Sheet с помощью Сервисного Аккаунта.
+
+Подробно о типах ключей в Google API (как их создавать и в чем разница) 
+я рассказывал в видео:
+[Как создать проект в Google Cloud Platform](https://www.youtube.com/watch?v=WpB42nS1uWE)
+
+### Полезные ссылки
+
+- https://console.cloud.google.com/
+- https://github.com/googleapis/google-api-python-client#installation
+
+- https://developers.google.com/sheets/api/guides/concepts
+- https://developers.google.com/sheets/api/quickstart/python
+- https://developers.google.com/sheets/api/guides/authorizing
+
+### Для установки
+
+`pip install -r req.txt`
